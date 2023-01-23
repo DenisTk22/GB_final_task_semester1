7f8a91c865f036c87be45e046cf012d298a9e4a4
Denis Tkachev <allpost-s@ya.ru>
2023-01-23 23:36:10 +0300

    file Блок-схема is added

--- Program.cs
+++ Program.cs
@@ -6,3 +6,5 @@ for (int i = 0; i < new Random().Next(0, 3); i++)
     array1[i] = array[new Random().Next(0, array.Length-1)];
 }
 Console.WriteLine($"{string.Join(" ",array1)}");
+
+//создание блок-схемы и загрузка в репозиторий.
