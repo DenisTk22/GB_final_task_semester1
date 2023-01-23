9a50f4358a5540fedf22fb094265b4f2f3a31673
Denis Tkachev <allpost-s@ya.ru>
2023-01-23 22:55:53 +0300

    title

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//
+﻿//Задача для финального контроля
 string [] array = new string [5] {"a", "-5", "Ostov", "9", "y"};
 string [] array1 = new string [3];
 for (int i = 0; i < new Random().Next(0, 3); i++)
