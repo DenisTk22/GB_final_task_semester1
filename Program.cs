eefe81c77168b869399a54b0bdac42886ed0277c
Denis Tkachev <allpost-s@ya.ru>
2023-01-23 22:54:42 +0300

    task is started

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//
+string [] array = new string [5] {"a", "-5", "Ostov", "9", "y"};
+string [] array1 = new string [3];
+for (int i = 0; i < new Random().Next(0, 3); i++)
+{
+    array1[i] = array[new Random().Next(0, array.Length-1)];
+}
+Console.WriteLine($"{string.Join(" ",array1)}");
