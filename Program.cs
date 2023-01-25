961b8df6deef2c52f574be626e2d12d024113001
Denis Tkachev <allpost-s@ya.ru>
2023-01-25 13:05:19 +0300

    task is finished

--- Program.cs
+++ Program.cs
@@ -1,10 +1,44 @@
-﻿//Задача для финального контроля
-string [] array = new string [5] {"a", "-5", "Ostov", "9", "y"};
-string [] array1 = new string [3];
-for (int i = 0; i < new Random().Next(0, 3); i++)
-{
-    array1[i] = array[new Random().Next(0, array.Length-1)];
-}
-Console.WriteLine($"{string.Join(" ",array1)}");
+﻿/*
+Задача для финального контроля.
+Написать программу, которая из имеющегося массива строк формирует массив из строк,
+длина которых меньше либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+лучше обойтись исключительно массивами. 
+*/
+// Задаю массив таким образом для быстроты контроля:
+string [] array = new string [] {"u8", "-5", "Ostov", ";)[", "y", "190"};
 
-//создание блок-схемы и загрузка в репозиторий.
+// Метод для подсчета количества элементов, размер которых меньше или равен трем символам в заданном массиве:
+int countEl (string [] massive)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <=3)
+                    {
+                        sum = sum + 1;
+                    }
+            }
+        return sum;
+    }
+int count = countEl (array);
+
+// Метод для заполнения искомого массива:
+string [] Newmas (string [] massive)
+{
+    string [] array1 = new string [count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+        {
+            if (massive[i].Length <=3)
+                {
+                    array1[j] = massive[i];
+                    j++;
+                }
+        }
+    return array1;
+}
+
+string [] Nmass = Newmas(array);
+
+Console.WriteLine($"Искомый массив: [{string.Join(", ", Nmass)}]");
